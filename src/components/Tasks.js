70b27fd73af4b62e818fcf31f6a0e49f62dd1e57
Akim Kingori <akim.king12@gmail.com>
2021-04-18 00:18:41 +0300

    Added Task component and refactored Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Task from './Task'
 
-
-const Tasks = ({tasks}) => {
+const Tasks = ({ tasks }) => {
 
 
 
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (
+                <Task key={task.id} task={task}/>
+            ))}
         </>
     )
 }
