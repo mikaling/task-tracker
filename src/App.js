72926f895167ea67f0bfc04c0d710e97fbd7ddb5
Akim Kingori <akim.king12@gmail.com>
2021-04-17 21:39:42 +0300

    Added styling and Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
+import Header from './components/Header'
 
 function App() {
   return (
     <div className='container'>
-      <h1>Hello from React</h1>
-      <h2>hi again</h2>
+      <Header title='Task Tracker'/>
     </div>
   );
 }
