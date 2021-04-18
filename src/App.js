9543f06db765a5af797d6c7db0bbbc097012d24b
Akim Kingori <akim.king12@gmail.com>
2021-04-18 20:43:48 +0300

    Added task reminder update on mock server

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ function App() {
     return data
   }
 
+  // Fetch single task
+  const fetchSingleTask = async (id) => {
+    const response = await fetch(`http://localhost:3001/tasks/${id}`)
+    const data = await response.json()
+
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
 
@@ -38,10 +46,10 @@ function App() {
         body: JSON.stringify(task)
       })
 
-      // Returns the new task once the POST request is successful
-      const data = await response.json()
+    // Returns the new task once the POST request is successful
+    const data = await response.json()
 
-      setTasks([...tasks, data])
+    setTasks([...tasks, data])
 
     // const id = Math.floor(Math.random() * 10000) + 1
     // const newTask = { id, ...task }
@@ -56,10 +64,30 @@ function App() {
   }
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchSingleTask(id)
+    // Change reminder value
+    const updatedTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+
+    // Create PUT request to update the object on the server
+    const response = await fetch(`http://localhost:3001/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await response.json()
+
+    // Set component's state to updated object from server
     setTasks(tasks.map((task) =>
       task.id === id ?
-        { ...task, reminder: !task.reminder } :
+        { ...task, reminder: data.reminder } :
         task))
   }
 
