bdec27faa5491fdc1af6916d247151b8101c3cc7
Akim Kingori <akim.king12@gmail.com>
2021-04-18 17:59:49 +0300

    Added reminder functionality

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'task 1',
+      text: 'Grocery shopping',
       day: 'Mar 3rd at 1:28 PM',
       reminder: false
     },
     {
       id: 2,
-      text: 'task 2',
+      text: 'Work meeting',
       day: 'Mar 6th at 1:08 AM',
       reminder: true
     },
     {
       id: 3,
-      text: 'task 3',
+      text: 'Doctor\'s appointment',
       day: 'Mar 4th at 3:28 PM',
       reminder: false
     },
@@ -30,11 +30,27 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ?
+        { ...task, reminder: !task.reminder } :
+        task))
+  }
+
+
 
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask }/> : 'No tasks available.' }
+      { tasks.length > 0 ?
+        <Tasks
+          onToggle={toggleReminder}
+          tasks={tasks}
+          onDelete={deleteTask}
+        /> :
+        'No tasks available.'
+      }
     </div>
   )
 }
