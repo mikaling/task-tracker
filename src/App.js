632ff72306d703f0e6d03f3f4ea7a38be819168b
Akim Kingori <akim.king12@gmail.com>
2021-04-18 19:56:56 +0300

    Added effect hook to fetch tasks from mock server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -8,6 +8,23 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }  
+
+    getTasks()
+  }, [])
+
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const response = await fetch('http://localhost:3001/tasks')
+    const data = await response.json()
+
+    return data
+  }
 
 
   // Add Task
