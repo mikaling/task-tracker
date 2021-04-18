2cedcbbdc4217ba66ac2e29d15dafa36eb3ddd37
Akim Kingori <akim.king12@gmail.com>
2021-04-18 17:35:53 +0300

    Added message if no tasks available

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      <Tasks tasks={tasks} onDelete={ deleteTask }/>
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask }/> : 'No tasks available.' }
     </div>
   )
 }
