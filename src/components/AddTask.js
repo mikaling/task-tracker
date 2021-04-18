26d9a0258526c6ca5b62c95726027874fa2383ed
Akim Kingori <akim.king12@gmail.com>
2021-04-18 18:36:03 +0300

    Added new task submission form

---
+++ src/components/AddTask.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const submitTask = (event) => {
+        event.preventDefault()
+
+        if (!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        // Clear the form
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
+
+    return (
+        <form className='add-form' onSubmit={submitTask}>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)} />
+            </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type='text' placeholder='Add Day & Time' value={day} onChange={(e) => setDay(e.target.value)} />
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <input
+                className='btn btn-block'
+                type='submit'
+                checked={reminder}
+                value='Save Task'
+            />
+        </form>
+    )
+}
+
+export default AddTask
